59f768746680cf87df7b00f060c8a5674e866f49
max <max@maxc.max>
2024-11-06 12:44:03 +0700

    next

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 
-print("Hello")
+from fastapi import FastAPI, Header
+from typing import Annotated
+
+app = FastAPI()
+
+@app.get("/")
+async def root(x_head: Annotated[str, Header()]):
+    return {"message": "Привет", "X-Head": x_head}
